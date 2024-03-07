aefc4706b978500cb49d4751f30c044da7d39716
Wosmerik Alexandr <shurik1777@gmail.com>
2024-03-07 23:21:33 +0300

    add env, hide TOKEN

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import asyncio
+import os
 
 from aiogram import Bot, Dispatcher, types
+from dotenv import find_dotenv, load_dotenv
 
-bot = Bot(token='')
+load_dotenv(find_dotenv())
+
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
 
